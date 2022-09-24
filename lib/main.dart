14c4de60e6b6c7a69a931a533aee5289009fdcf9
bumis95 <bumis95@gmail.com>
2022-09-24 17:48:30 +0300

    Add bottom nav

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 import './screens/home_screen.dart';
@@ -16,11 +17,38 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       translations: AppTranslation(),
-      locale: Get.deviceLocale,
+      locale: const Locale('en', 'US'),
       fallbackLocale: const Locale('en', 'US'),
       title: 'app_name'.tr,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        appBarTheme: const AppBarTheme(
+          systemOverlayStyle: SystemUiOverlayStyle(
+            statusBarColor: Colors.white,
+            statusBarIconBrightness: Brightness.dark,
+          ),
+          color: Colors.white,
+          foregroundColor: Colors.black,
+          iconTheme: IconThemeData(
+            color: Colors.black,
+          ),
+          elevation: 0,
+        ),
+        colorScheme: const ColorScheme.light(
+          primary: Colors.green,
+          onPrimary: Colors.green,
+          secondary: Colors.red,
+        ),
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.white,
+          selectedItemColor: Colors.black,
+          unselectedItemColor: Colors.black,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          elevation: 5,
+          selectedIconTheme: IconThemeData(size: 30),
+          unselectedIconTheme: IconThemeData(size: 30),
+        ),
       ),
       home: const HomeScreen(),
     );
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,8 +9,43 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('app_name'.tr),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add_box_outlined),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.send),
+          ),
+        ],
       ),
       body: Container(),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_box_outlined),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_outline),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.circle_outlined),
+            label: '',
+          ),
+        ],
+      ),
     );
   }
 }
