32bc83d96aeba5e7eaa44356be5c81d7f01d6ebc
bumis95 <bumis95@gmail.com>
2022-09-26 15:20:17 +0300

    Replace theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 import './screens/home_screen.dart';
+import './utils/app_theme.dart';
 import './utils/app_translation.dart';
 
 void main() {
@@ -20,36 +20,7 @@ class MyApp extends StatelessWidget {
       locale: const Locale('en', 'US'),
       fallbackLocale: const Locale('en', 'US'),
       title: 'app_name'.tr,
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(
-          systemOverlayStyle: SystemUiOverlayStyle(
-            statusBarColor: Colors.white,
-            statusBarIconBrightness: Brightness.dark,
-          ),
-          color: Colors.white,
-          foregroundColor: Colors.black,
-          iconTheme: IconThemeData(
-            color: Colors.black,
-          ),
-          elevation: 0,
-        ),
-        colorScheme: const ColorScheme.light(
-          primary: Colors.green,
-          onPrimary: Colors.green,
-          secondary: Colors.red,
-        ),
-        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-          type: BottomNavigationBarType.fixed,
-          backgroundColor: Colors.white,
-          selectedItemColor: Colors.black,
-          unselectedItemColor: Colors.black,
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-          elevation: 5,
-          selectedIconTheme: IconThemeData(size: 30),
-          unselectedIconTheme: IconThemeData(size: 30),
-        ),
-      ),
+      theme: AppTheme.lightTheme,
       home: const HomeScreen(),
     );
   }
---
+++ lib/utils/app_theme.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class AppTheme {
+  static final ThemeData lightTheme = ThemeData(
+    appBarTheme: const AppBarTheme(
+      systemOverlayStyle: SystemUiOverlayStyle(
+        statusBarColor: Colors.white,
+        statusBarIconBrightness: Brightness.dark,
+      ),
+      color: Colors.white,
+      foregroundColor: Colors.black,
+      iconTheme: IconThemeData(
+        color: Colors.black,
+      ),
+      elevation: 0,
+    ),
+    colorScheme: const ColorScheme.light(
+      primary: Colors.green,
+      onPrimary: Colors.green,
+      secondary: Colors.red,
+    ),
+    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+      type: BottomNavigationBarType.fixed,
+      backgroundColor: Colors.white,
+      selectedItemColor: Colors.black,
+      unselectedItemColor: Colors.black,
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
+      elevation: 5,
+      selectedIconTheme: IconThemeData(size: 30),
+      unselectedIconTheme: IconThemeData(size: 30),
+    ),
+  );
+}
